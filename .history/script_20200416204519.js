8ae9bd8c35f4ff95c586f1d770a91f4af36784d3
Laurent Capello <laurent.capello@gmail.com>
2020-04-17 19:27:00 +0200

    boh

---
+++ .history/script_20200416204519.js
@@ -0,0 +1,18 @@
+const grid = document.querySelector(`#grid-div`)
+const squaresPerSide = 50
+for (let i = 1; i <= squaresPerSide; i++) {
+  for (let j = 1; j <= squaresPerSide; j++) {
+    const cell = document.createElement(`div`)
+    cell.classList.add(`cell`)
+    grid.appendChild(cell)
+  }
+  const br = document.createElement(`br`)
+  grid.appendChild(br)
+}
+
+//make sure the user input an integer
+function getSizeFromUser() {
+  let size
+  do { size = Number(prompt(`How many squares per side?`)) }while (isNaN(size) || size == ``)
+  return size
+}
